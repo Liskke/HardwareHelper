ad71db32813633e4a9c0c0d5aa746485d64f137b
dell <dell@LISKE-LAPTOP>
2026-05-03 17:26:59 +0200

    Dodaj pliki projektów.

---
+++ HardwareHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HardwareHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
