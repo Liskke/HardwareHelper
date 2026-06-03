b6419bc0cf0222f722c273e59dfb6cbf16ceb8c9
Kacper Mielczarek <pzx119559@student.poznan.merito.pl>
2026-06-03 11:08:31 +0200

    funkcja: dodanie automatycznego przekierowania użytkowników na podstawie ról

--- HardwareHelper/Controllers/HomeController.cs
+++ HardwareHelper/Controllers/HomeController.cs
@@ -1,36 +1,50 @@
-using HardwareHelper.Models;
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HardwareHelper.Models;
 
-namespace HardwareHelper.Controllers
+namespace HardwareHelper.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
+    public IActionResult Index()
+    {
+        // 1. Jeśli użytkownik NIE jest zalogowany, przekieruj go bezpośrednio do strony logowania ASP.NET Core Identity
+        if (!User.Identity.IsAuthenticated)
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return RedirectToPage("/Account/Login", new { area = "Identity" });
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // 2. Jeśli zalogowany użytkownik ma rolę Admina lub Serwisanta, przekieruj go do Twojego panelu serwisanta
+        if (User.IsInRole("Admin") || User.IsInRole("Serwisant"))
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return RedirectToAction("Index", "Serwisant");
         }
-        public IActionResult BrakDostepu()
+
+        // 3. Jeśli zalogowany użytkownik ma rolę Klienta, przekieruj go do panelu klienta
+        if (User.IsInRole("Klient"))
         {
-            return View();
+            return RedirectToAction("MojeZgloszenia", "Klient");
         }
+
+        // Domyślny widok awaryjny (np. jeśli użytkownik nie ma przypisanej żadnej roli)
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
